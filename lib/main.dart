7aee5c7ba22630b6c639241ddde7c4fe79f54e24
OHR7 <oherrera@uabc.edu.mx>
2019-06-18 11:03:47 -0700

    - Added card swiper widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movies_app/src/pages/home_page.dart';
 
  
 void main() => runApp(MyApp());
@@ -7,10 +8,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Movies App',
       initialRoute: '/',
       routes: {
-        '/': 
+        '/': (BuildContext context ) => HomePage(),
       },
     );
   }
